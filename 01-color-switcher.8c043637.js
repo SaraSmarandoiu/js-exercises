fd4c6ac6aba54221632bd8401b0a0af1f515165e
SaraSmarandoiu <151066427+SaraSmarandoiu@users.noreply.github.com>
2024-05-06 16:24:45 +0000

    Deploying to gh-pages from @ SaraSmarandoiu/goit-js-hw-09@1a66eb1082e3baa9e8797c8fa3fa4c32984a7235 🚀

---
+++ 01-color-switcher.8c043637.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");function n(){document.body.style.backgroundColor="#"+Math.floor(16777215*Math.random()).toString(16)}e.addEventListener("click",(function(){e.disabled=!0,d.disabled=!1,t=setInterval(n,1e3)})),d.addEventListener("click",(function(){e.disabled=!1,d.disabled=!0,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.8c043637.js.map
